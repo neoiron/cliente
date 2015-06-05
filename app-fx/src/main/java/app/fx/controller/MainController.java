0217b4dea06b879bb5b0acd0d0ce098aa22cc28d
Prestes, E. M <neoiron@gmail.com>
2015-06-05 19:23:39 -0300

    Criado comportamento 'onClickMunicipioMenu'.

--- app-fx/src/main/java/app/fx/controller/MainController.java
+++ app-fx/src/main/java/app/fx/controller/MainController.java
@@ -1,9 +1,17 @@
 package app.fx.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class MainController extends AbstractController {
 
     public static final String STYLE_URL = "/app/fx/view/main.css";
 
     public static final String VIEW_URL = "/app/fx/view/main.fxml";
     public static final String VIEW_TITLE = "Cadastro de Clientes";
+
+    @FXML
+    public void onClickMunicipioMenu(ActionEvent event) {
+        System.out.println("Menu município funcionando!");
+    }
 }
