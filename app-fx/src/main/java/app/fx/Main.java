201209eaa48e9b086f643e5e49b8a4c069299f08
Prestes, E. M <neoiron@gmail.com>
2015-06-05 19:09:59 -0300

    Ajustes em constantes para View.

--- app-fx/src/main/java/app/fx/Main.java
+++ app-fx/src/main/java/app/fx/Main.java
@@ -5,15 +5,19 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static app.fx.controller.MainController.VIEW_URL;
+import static app.fx.controller.MainController.VIEW_TITLE;
+import static app.fx.controller.MainController.STYLE_URL;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Scene scene = FXMLHelper.createScene(
                 primaryStage,
-                "Cadastro de Clientes",
-                "/app/fx/view/main.fxml",
-                "/app/fx/view/main.css");
+                VIEW_TITLE,
+                VIEW_URL,
+                STYLE_URL);
 
         primaryStage.setScene(scene);
         primaryStage.show();
--- app-fx/src/main/java/app/fx/controller/MainController.java
+++ app-fx/src/main/java/app/fx/controller/MainController.java
@@ -2,4 +2,8 @@ package app.fx.controller;
 
 public class MainController extends AbstractController {
 
+    public static final String STYLE_URL = "/app/fx/view/main.css";
+
+    public static final String VIEW_URL = "/app/fx/view/main.fxml";
+    public static final String VIEW_TITLE = "Cadastro de Clientes";
 }
