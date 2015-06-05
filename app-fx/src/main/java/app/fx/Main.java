d620f15d7de9b34968389e21d26ee9adb627e473
Prestes, E. M <neoiron@gmail.com>
2015-06-05 17:52:36 -0300

    Criada view principal com menu de acesso aos cadastros do sistema.

--- app-fx/src/main/java/app/fx/Main.java
+++ app-fx/src/main/java/app/fx/Main.java
@@ -1,5 +1,36 @@
 package app.fx;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.net.URL;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxml;
+        URL view, style;
+        Parent root;
+        Scene scene;
+
+        view = this.getClass().getResource("/app/fx/view/main.fxml");
+        style = this.getClass().getResource("/app/fx/view/main.css");
+        fxml = new FXMLLoader(view);
+        root = fxml.load();
+        scene = new Scene(root);
+
+        scene.getStylesheets().add(style.toExternalForm());
+
+        primaryStage.setTitle("Cadastro de Clientes");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
