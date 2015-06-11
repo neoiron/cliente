6faeb040429a09894dd103488d634fcd7be809e1
Prestes, E. M <neoiron@gmail.com>
2015-06-11 12:31:24 -0300

    Criado e configurado ContextMenu.
    Com MenuItem 'atualizar'.

--- app-fx/src/main/java/app/fx/controller/MunicipioController.java
+++ app-fx/src/main/java/app/fx/controller/MunicipioController.java
@@ -1,6 +1,9 @@
 package app.fx.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
 
 public class MunicipioController extends AbstractController {
 
@@ -8,6 +11,11 @@ public class MunicipioController extends AbstractController {
 
     public static final String VIEW_URL = "/app/fx/view/municipio.fxml";
     public static final String VIEW_TITLE = "Cadastro de Municípios";
+
+    @FXML
+    private TableView<?> tvMUNICIPIOS;
+
+    private ContextMenu cm;
 
     @FXML
     public void initialize() {
@@ -20,6 +28,12 @@ public class MunicipioController extends AbstractController {
     }
 
     private void initView() {
-        
+        MenuItem mi;
+
+        cm = new ContextMenu();
+        mi = new MenuItem("Atualizar");
+        cm.getItems().add(0, mi);
+
+        tvMUNICIPIOS.setContextMenu(cm);
     }
 }
