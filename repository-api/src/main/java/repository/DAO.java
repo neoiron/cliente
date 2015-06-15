18aa4062ecb9796ac2d497ddc8174e01f11e0078
Prestes, E. M. <neoiron@gmail.com>
2015-06-15 15:07:45 -0300

    Incluídos comportamentos 'inserir', 'atualizar' e 'apagar'.

--- repository-api/src/main/java/repository/DAO.java
+++ repository-api/src/main/java/repository/DAO.java
@@ -1,4 +1,10 @@
 package repository;
 
 interface DAO<T, E extends Throwable> {
+
+    void inserir(T domain) throws E;
+
+    void atualizar(T domain) throws E;
+
+    void apagar(T domain) throws E;
 }
