cc3c8e820b9015707f25e700e2a3f0186cc87445
Prestes, E. M. <neoiron@gmail.com>
2015-06-15 15:13:17 -0300

    Incluídos os construtores.

--- core/src/main/java/domain/exception/MunicipioException.java
+++ core/src/main/java/domain/exception/MunicipioException.java
@@ -4,4 +4,15 @@ public class MunicipioException extends Exception {
 
     private static final long serialVersionUID = -2194177049299589050L;
 
+    public MunicipioException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MunicipioException(String message) {
+        super(message);
+    }
+
+    public MunicipioException(Throwable cause) {
+        super(cause);
+    }
 }
