001d3fbdeaae02c4ee8dd16208a4f0e734ffdfb1
Prestes, E. M. <neoiron@gmail.com>
2015-06-15 15:12:16 -0300

    Criada a exception para município.

--- core/src/main/java/domain/exception/MunicipioException.java
+++ core/src/main/java/domain/exception/MunicipioException.java
@@ -1,5 +1,7 @@
 package domain.exception;
 
-public class MunicipioException {
+public class MunicipioException extends Exception {
+
+    private static final long serialVersionUID = -2194177049299589050L;
 
 }
