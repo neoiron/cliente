9adcb2d8bb0c6dc2314420ee9dbcdd4a5a95fd84
Prestes, E. M. <neoiron@gmail.com>
2015-06-23 10:31:01 -0300

    Criados comportamentos 'isSelecionado' e 'isNotSelecionado'.

--- core/src/main/java/domain/model/UFVO.java
+++ core/src/main/java/domain/model/UFVO.java
@@ -1,6 +1,13 @@
 package domain.model;
 
 public enum UFVO {
-    ESCOLHA, AC, AL, AM, AP, BA, CE, DF, ES, GO, MA, MT, MS, MG, PA, PB, PR, PE, PI, RJ, RN, RS, RO, RR, SC, SP, SE, TO;
+    SELECIONE, AC, AL, AM, AP, BA, CE, DF, ES, GO, MA, MT, MS, MG, PA, PB, PR, PE, PI, RJ, RN, RS, RO, RR, SC, SP, SE, TO;
 
+    public boolean isSelecionado() {
+        return !SELECIONE.equals(this);
+    }
+
+    public boolean isNotSelecionado() {
+        return !isSelecionado();
+    }
 }
