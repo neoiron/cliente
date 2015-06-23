58af92f74aa37dd567e73070016ae3c398423870
Prestes, E. M. <neoiron@gmail.com>
2015-06-23 12:34:53 -0300

    Ajuste em interface 'Service'.

--- service-api/src/main/java/service/Service.java
+++ service-api/src/main/java/service/Service.java
@@ -2,7 +2,7 @@ package service;
 
 import domain.model.Entidade;
 
-interface Service<T extends Entidade<?>, E extends Exception> {
+public interface Service<T extends Entidade<?>, E extends Exception> {
 
     void validar(T domain) throws E;
 
