6be35d8cae69797f628f19646eb7c32c6038b137
Prestes, E. M. <neoiron@gmail.com>
2015-06-23 12:53:12 -0300

    Definida composição com interface 'DAO'.

--- service-impl/src/main/java/service/impl/AbstractService.java
+++ service-impl/src/main/java/service/impl/AbstractService.java
@@ -1,7 +1,15 @@
 package service.impl;
 
+import repository.DAO;
 import domain.model.Entidade;
 
 abstract class AbstractService<T extends Entidade<?>, E extends Exception> {
 
+    protected final DAO<T, E> dao;
+
+    public AbstractService(DAO<T, E> dao) {
+        super();
+
+        this.dao = dao;
+    }
 }
