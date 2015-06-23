a8942a437e7bc78b37a65496cf596a6d409d0da7
Prestes, E. M. <neoiron@gmail.com>
2015-06-23 10:37:09 -0300

    Sobreescritos comportamentos 'hashCode' e 'equals'.

--- core/src/main/java/domain/model/Municipio.java
+++ core/src/main/java/domain/model/Municipio.java
@@ -22,6 +22,40 @@ public class Municipio {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((uf == null) ? 0 : uf.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Municipio)) {
+            return false;
+        }
+        Municipio other = (Municipio) obj;
+        if (nome == null) {
+            if (other.nome != null) {
+                return false;
+            }
+        } else if (!nome.equals(other.nome)) {
+            return false;
+        }
+        if (uf != other.uf) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
     public String toString() {
         return String.format("Municipio [nome=%s, uf=%s, %s]", nome, uf, super.toString());
     }
