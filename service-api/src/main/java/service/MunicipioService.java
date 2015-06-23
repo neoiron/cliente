4974e835990aa3e4fa38bc75a422422174e3af92
Prestes, E. M. <neoiron@gmail.com>
2015-06-23 12:15:47 -0300

    Criado comportamento 'listarInvertido'.

--- service-api/src/main/java/service/MunicipioService.java
+++ service-api/src/main/java/service/MunicipioService.java
@@ -1,6 +1,7 @@
 package service;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 import domain.exception.MunicipioException;
 import domain.model.Municipio;
@@ -9,4 +10,12 @@ import domain.model.UFVO;
 public interface MunicipioService extends Service<Municipio, MunicipioException> {
 
     Collection<Municipio> listar(UFVO uf) throws MunicipioException;
+
+    default Collection<Municipio> listarInvertido(UFVO uf) throws MunicipioException {
+        Collection<Municipio> municipios = listar(uf);
+
+        return municipios.stream()
+                .sorted((m1, m2) -> -m1.compareTo(m2))
+                .collect(Collectors.toSet());
+    }
 }
