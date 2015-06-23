cd3a74559e00ca13d3fdc216e6504bca3427cc85
Prestes, E. M. <neoiron@gmail.com>
2015-06-23 14:28:21 -0300

    Incluído suporte ao 'MunicipioDAO'.

--- service-impl/src/main/java/service/impl/DefaultMunicipioService.java
+++ service-impl/src/main/java/service/impl/DefaultMunicipioService.java
@@ -1,11 +1,20 @@
 package service.impl;
 
+import repository.MunicipioDAO;
 import domain.exception.MunicipioException;
 import domain.model.Municipio;
 
 public class DefaultMunicipioService extends AbstractService<Municipio, MunicipioException> {
 
-    protected DefaultMunicipioService() {
-        super(null);
+    private MunicipioDAO dao;
+
+    public DefaultMunicipioService() {
+        this(null); // FIXME new DAO aqui.
+    }
+
+    protected DefaultMunicipioService(MunicipioDAO dao) {
+        super(dao);
+
+        this.dao = dao;
     }
 }
