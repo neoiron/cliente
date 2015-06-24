d81ee3e163f3281b185c9fb5a91b52bba9111d64
Prestes, E. M. <neoiron@gmail.com>
2015-06-24 16:12:24 -0300

    Implementado comportamento 'inserir'.

--- repository-jdbc/src/main/java/repository/jdbc/AbstractDAO.java
+++ repository-jdbc/src/main/java/repository/jdbc/AbstractDAO.java
@@ -1,17 +1,43 @@
 package repository.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import domain.model.Entidade;
 import repository.DAO;
+import repository.exception.DatabaseException;
 
 abstract class AbstractDAO<T extends Entidade<?, E>, E extends Throwable> implements DAO<T, E> {
 
+    protected abstract E getFailInsert();
     protected abstract E getExceptionInsert();
     protected abstract String getSQLInsert();
-    protected abstract void prepareStatementInsert();
+    protected abstract void prepareStatementInsert(PreparedStatement query);
 
     @Override
     public void inserir(T domain) throws E {
-        
+        Connection c;
+        PreparedStatement ps;
+
+        try {
+            c = DataSource.openConnection();
+            ps = c.prepareStatement(getSQLInsert());
+
+            try {
+                prepareStatementInsert(ps);
+
+                int rows = ps.executeUpdate();
+
+                if (rows == 0) 
+                    throw getFailInsert();
+            } finally {
+                DataSource.close(ps);
+                DataSource.close(c);
+            }
+        } catch (SQLException | DatabaseException cause) {
+            throw getExceptionInsert();
+        }
     }
 
     protected abstract E getExceptionUpdate();
