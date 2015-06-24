dc151fe15036545349604366b4722877442c25ea
Prestes, E. M. <neoiron@gmail.com>
2015-06-24 15:26:32 -0300

    Criados comportamentos para 'inserir' uma entidade.

--- repository-jdbc/src/main/java/repository/jdbc/AbstractDAO.java
+++ repository-jdbc/src/main/java/repository/jdbc/AbstractDAO.java
@@ -5,4 +5,12 @@ import repository.DAO;
 
 abstract class AbstractDAO<T extends Entidade<?, E>, E extends Throwable> implements DAO<T, E> {
 
+    protected abstract E getExceptionInsert();
+    protected abstract String getSQLInsert();
+    protected abstract void prepareStatementInsert();
+
+    @Override
+    public void inserir(T domain) throws E {
+        
+    }
 }
