33e354ae6bf4ef6843102fba59416cc6895a0073
Prestes, E. M. <neoiron@gmail.com>
2015-06-24 15:18:14 -0300

    Criada interface para usar dados de arquivo properties.

--- repository-jdbc/src/main/java/repository/jdbc/DataSource.java
+++ repository-jdbc/src/main/java/repository/jdbc/DataSource.java
@@ -6,6 +6,14 @@ import repository.exception.DatabaseException;
 
 final class DataSource {
 
+    interface JDBC {
+        String FILE_NAME = "jdbc.properties";
+        String DRIVER = "jdbc.driver";
+        String URL = "jdbc.driver";
+        String USER = "jdbc.user";
+        String PASSWORD = "jdbc.password";
+    }
+
     private DataSource() {
         super();
     }
