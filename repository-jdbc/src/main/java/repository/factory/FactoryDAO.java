b6a6202393fc7c60d0e59f4c4237a04426fb7b61
Prestes, E. M. <neoiron@gmail.com>
2015-06-24 16:58:49 -0300

    Criado comportamento 'newInstance'.

--- repository-jdbc/src/main/java/repository/factory/FactoryDAO.java
+++ repository-jdbc/src/main/java/repository/factory/FactoryDAO.java
@@ -1,8 +1,16 @@
 package repository.factory;
 
+import repository.DAO;
+import domain.model.Entidade;
+
 abstract class FactoryDAO {
 
     protected FactoryDAO() {
         super();
+    }
+
+    protected static <T extends Entidade<?, E>, E extends Throwable> DAO<T, E> newInstance(Class<DAO<T, E>> _class) 
+            throws InstantiationException, IllegalAccessException {
+        return _class.newInstance();
     }
 }
