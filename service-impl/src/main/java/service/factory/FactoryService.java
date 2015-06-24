aab1511959e16dafe96c27f0af840d49d7594df1
Prestes, E. M. <neoiron@gmail.com>
2015-06-24 19:09:47 -0300

    Criado comportamento 'createMunicipioService'.

--- service-impl/src/main/java/service/factory/FactoryService.java
+++ service-impl/src/main/java/service/factory/FactoryService.java
@@ -1,8 +1,15 @@
 package service.factory;
 
+import service.MunicipioService;
+import service.impl.DefaultMunicipioService;
+
 public final class FactoryService {
 
     private FactoryService() {
         super();
+    }
+
+    public static MunicipioService createMunicipioService() {
+        return new DefaultMunicipioService();
     }
 }
