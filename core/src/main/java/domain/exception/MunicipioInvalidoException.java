d97b957df0262ffdf31adcc675f48962efa40f5a
Prestes, E. M. <neoiron@gmail.com>
2015-06-24 20:05:01 -0300

    Definido construtor com mensagem.

--- core/src/main/java/domain/exception/MunicipioInvalidoException.java
+++ core/src/main/java/domain/exception/MunicipioInvalidoException.java
@@ -1,5 +1,10 @@
 package domain.exception;
 
-public class MunicipioInvalidoException {
+public class MunicipioInvalidoException extends MunicipioException {
 
+    private static final long serialVersionUID = 6405222188828268110L;
+
+    public MunicipioInvalidoException(String message) {
+        super(message);
+    }
 }
