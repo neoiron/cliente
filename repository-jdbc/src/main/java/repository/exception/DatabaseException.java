ab691f781551d40fa697f25ee4b61cdbc5b6a6f8
Prestes, E. M. <neoiron@gmail.com>
2015-06-24 12:20:43 -0300

    Ajuste em 'DatabaseException'.

--- repository-jdbc/src/main/java/repository/exception/DatabaseException.java
+++ repository-jdbc/src/main/java/repository/exception/DatabaseException.java
@@ -1,5 +1,7 @@
 package repository.exception;
 
-public class DatabaseException {
+public class DatabaseException extends Exception {
+
+    private static final long serialVersionUID = -3441942591814520080L;
 
 }
