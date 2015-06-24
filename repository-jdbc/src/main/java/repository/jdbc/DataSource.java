af07e3a10851d4c2a93a70bbeba7e552cb9092e5
Prestes, E. M. <neoiron@gmail.com>
2015-06-24 15:19:59 -0300

    Implementado comportamento 'openConnection'.

--- repository-jdbc/src/main/java/repository/jdbc/DataSource.java
+++ repository-jdbc/src/main/java/repository/jdbc/DataSource.java
@@ -1,6 +1,12 @@
 package repository.jdbc;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 import repository.exception.DatabaseException;
 
@@ -19,7 +25,29 @@ final class DataSource {
     }
 
     public static Connection openConnection() throws DatabaseException {
-        return null;
+        Properties p = new Properties();
+        try (Reader r = new FileReader(JDBC.FILE_NAME)) {
+            p.load(r);
+
+            Class.forName(p.getProperty(JDBC.DRIVER));
+
+            return DriverManager.getConnection(
+                    p.getProperty(JDBC.URL), 
+                    p.getProperty(JDBC.USER),
+                    p.getProperty(JDBC.PASSWORD));
+        } catch (SQLException cause) {
+            throw new DatabaseException(
+                    "PROBLEMAS AO ABRIR CONEXÃO COM BANCO DE DADOS!", 
+                    cause);
+        } catch (IOException cause) {
+            throw new DatabaseException(
+                    "PROBLEMAS AO ABRIR 'jdbc.properties'!", 
+                    cause);
+        } catch (ClassNotFoundException cause) {
+            throw new DatabaseException(
+                    "PROBLEMAS AO CARREGAR O DRIVER!", 
+                    cause);
+        }
     }
 
     public static void close(Connection connection) throws DatabaseException {
