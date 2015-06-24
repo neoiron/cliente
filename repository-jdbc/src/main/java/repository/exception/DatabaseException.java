906c4536bf519c46070c69da300e8b6377b5e88e
Prestes, E. M. <neoiron@gmail.com>
2015-06-24 12:21:27 -0300

    Definido construtor.

--- repository-jdbc/src/main/java/repository/exception/DatabaseException.java
+++ repository-jdbc/src/main/java/repository/exception/DatabaseException.java
@@ -4,4 +4,7 @@ public class DatabaseException extends Exception {
 
     private static final long serialVersionUID = -3441942591814520080L;
 
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
