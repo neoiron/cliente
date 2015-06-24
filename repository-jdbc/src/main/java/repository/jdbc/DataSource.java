a24ccd270a495a674f1fe718df7a88d615f45119
Prestes, E. M. <neoiron@gmail.com>
2015-06-24 12:25:41 -0300

    Definido comportamento 'openConnection'.

--- repository-jdbc/src/main/java/repository/jdbc/DataSource.java
+++ repository-jdbc/src/main/java/repository/jdbc/DataSource.java
@@ -1,8 +1,16 @@
 package repository.jdbc;
 
+import java.sql.Connection;
+
+import repository.exception.DatabaseException;
+
 final class DataSource {
 
     private DataSource() {
         super();
+    }
+
+    public static Connection openConnection() throws DatabaseException {
+        return null;
     }
 }
