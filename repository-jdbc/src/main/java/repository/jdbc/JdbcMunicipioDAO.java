c4dc3acf429d96e5e37910a6c8bae3f94a210835
Prestes, E. M. <neoiron@gmail.com>
2015-06-24 16:21:56 -0300

    Incluído suporte a 'AbstractDAO'.

--- repository-jdbc/src/main/java/repository/jdbc/JdbcMunicipioDAO.java
+++ repository-jdbc/src/main/java/repository/jdbc/JdbcMunicipioDAO.java
@@ -1,5 +1,69 @@
 package repository.jdbc;
 
-public class JdbcMunicipioDAO {
+import java.sql.PreparedStatement;
 
+import domain.exception.MunicipioException;
+import domain.model.Municipio;
+
+public class JdbcMunicipioDAO extends AbstractDAO<Municipio, MunicipioException> {
+
+    @Override
+    protected MunicipioException getFailInsert() {
+        return null;
+    }
+
+    @Override
+    protected MunicipioException getExceptionInsert() {
+        return null;
+    }
+
+    @Override
+    protected String getSQLInsert() {
+        return null;
+    }
+
+    @Override
+    protected void prepareStatementInsert(PreparedStatement query) {
+        
+    }
+
+    @Override
+    protected MunicipioException getFailUpdate() {
+        return null;
+    }
+
+    @Override
+    protected MunicipioException getExceptionUpdate() {
+        return null;
+    }
+
+    @Override
+    protected String getSQLUpdate() {
+        return null;
+    }
+
+    @Override
+    protected void prepareStatementUpdate(PreparedStatement query) {
+        
+    }
+
+    @Override
+    protected MunicipioException getFailDelete() {
+        return null;
+    }
+
+    @Override
+    protected MunicipioException getExceptionDelete() {
+        return null;
+    }
+
+    @Override
+    protected String getSQLDelete() {
+        return null;
+    }
+
+    @Override
+    protected void prepareStatementDelete(PreparedStatement ps) {
+        
+    }
 }
