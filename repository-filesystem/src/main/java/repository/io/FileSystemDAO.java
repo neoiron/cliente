be07f248acb10993ea94f30e595e7ce25822b74e
Prestes, E. M. <neoiron@gmail.com>
2015-06-25 11:31:00 -0300

    Implementado comportamento 'inserir'.

--- repository-filesystem/src/main/java/repository/io/FileSystemDAO.java
+++ repository-filesystem/src/main/java/repository/io/FileSystemDAO.java
@@ -1,13 +1,27 @@
 package repository.io;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 import repository.DAO;
 import domain.Domain;
 
 abstract class FileSystemDAO<T extends Domain<E>, E extends Throwable> implements DAO<T, E> {
 
+    public static final char CSV_SEPARATOR = ';';
+    protected abstract String getFileName();
+    protected abstract E getExceptionInsert(Throwable cause);
+
     @Override
     public void inserir(T domain) throws E {
-        
+        try (RandomAccessFile source = DataSource.openWriteableFile(getFileName())) {
+            long pos = source.length() - 1;
+            source.seek(pos);
+
+            source.write(domain.toCSV(CSV_SEPARATOR).getBytes());
+        } catch (IOException cause) {
+            throw getExceptionInsert(cause);
+        }
     }
 
     @Override
