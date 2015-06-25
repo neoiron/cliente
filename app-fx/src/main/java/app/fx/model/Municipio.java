1491ac757b83e3a8d3a10e7b3c3413ccd8a41db2
Prestes, E. M. <neoiron@gmail.com>
2015-06-25 17:12:35 -0300

    Definido construtor.

--- app-fx/src/main/java/app/fx/model/Municipio.java
+++ app-fx/src/main/java/app/fx/model/Municipio.java
@@ -1,5 +1,7 @@
 package app.fx.model;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableObjectValue;
 import javafx.beans.value.ObservableStringValue;
 import domain.model.UFVO;
@@ -9,4 +11,12 @@ public class Municipio {
     private ObservableObjectValue<domain.model.Municipio> domain;
     private ObservableStringValue nome;
     private ObservableObjectValue<UFVO> uf;
+
+    public Municipio(domain.model.Municipio domain) {
+        super();
+
+        this.domain = new SimpleObjectProperty<>(domain);
+        this.nome = new SimpleStringProperty(String.valueOf(domain.getNome()));
+        this.uf = new SimpleObjectProperty<>(domain.getUf());
+    }
 }
