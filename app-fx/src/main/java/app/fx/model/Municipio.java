50ae2a56563771d1116ee2bf46f822a0990ef666
Prestes, E. M. <neoiron@gmail.com>
2015-06-25 17:12:05 -0300

    Definidos atributos de 'Municipio'.

--- app-fx/src/main/java/app/fx/model/Municipio.java
+++ app-fx/src/main/java/app/fx/model/Municipio.java
@@ -1,5 +1,12 @@
 package app.fx.model;
 
+import javafx.beans.value.ObservableObjectValue;
+import javafx.beans.value.ObservableStringValue;
+import domain.model.UFVO;
+
 public class Municipio {
 
+    private ObservableObjectValue<domain.model.Municipio> domain;
+    private ObservableStringValue nome;
+    private ObservableObjectValue<UFVO> uf;
 }
