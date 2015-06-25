82e369ba0ac9dead2fb630074b164f853a547cdb
Prestes, E. M. <neoiron@gmail.com>
2015-06-25 16:32:19 -0300

    Removidos arquivos desnecessários.

--- repository-filesystem/src/main/java/repository/io/DataSource.java
+++ repository-filesystem/src/main/java/repository/io/DataSource.java
@@ -9,8 +9,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
 
-import repository.exception.FileSystemException;
-
 final class DataSource {
 
     interface Property {
@@ -41,12 +39,8 @@ final class DataSource {
         return new RandomAccessFile(path.toFile(), Property.Permission.RW);
     }
 
-    public static void close(RandomAccessFile file) throws FileSystemException {
-        try {
-            if (file != null)
-                file.close();
-        } catch (IOException cause) {
-            throw new FileSystemException("PROBLEMAS AO FECHAR ARQUIVO", cause);
-        }
+    public static void close(RandomAccessFile file) throws IOException {
+        if (file != null)
+            file.close();
     }
 }
