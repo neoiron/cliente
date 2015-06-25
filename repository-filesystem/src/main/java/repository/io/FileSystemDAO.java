fa680d7a1881ef4572d0d18b7940d75aa0c1d62b
Prestes, E. M. <neoiron@gmail.com>
2015-06-25 12:55:14 -0300

    Ajustes em 'FileSystemDAO'.

--- repository-filesystem/src/main/java/repository/io/FileSystemDAO.java
+++ repository-filesystem/src/main/java/repository/io/FileSystemDAO.java
@@ -1,36 +1,8 @@
 package repository.io;
 
-import java.io.IOException;
-import java.io.RandomAccessFile;
-
-import repository.DAO;
-import domain.Domain;
-
-abstract class FileSystemDAO<T extends Domain<E>, E extends Throwable> implements DAO<T, E> {
+abstract class FileSystemDAO {
 
     public static final char CSV_SEPARATOR = ';';
-    protected abstract String getFileName();
-    protected abstract E getExceptionInsert(Throwable cause);
 
-    @Override
-    public void inserir(T domain) throws E {
-        try (RandomAccessFile source = DataSource.openWriteableFile(getFileName())) {
-            long pos = source.length() - 1;
-            source.seek(pos);
-
-            source.write(domain.toCSV(CSV_SEPARATOR).getBytes());
-        } catch (IOException cause) {
-            throw getExceptionInsert(cause);
-        }
-    }
-
-    @Override
-    public void atualizar(T domain) throws E {
-        
-    }
-
-    @Override
-    public void apagar(T domain) throws E {
-        
-    }
+    public static final long FILE_BEGIN = 0L;
 }
