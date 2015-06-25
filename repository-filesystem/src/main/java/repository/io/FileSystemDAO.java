bba1c052707694ed7fd7f56861634a2b426c7931
Prestes, E. M. <neoiron@gmail.com>
2015-06-25 11:08:26 -0300

    Incluído comportamento 'inserir'.

--- repository-filesystem/src/main/java/repository/io/FileSystemDAO.java
+++ repository-filesystem/src/main/java/repository/io/FileSystemDAO.java
@@ -1,5 +1,12 @@
 package repository.io;
 
-abstract class FileSystemDAO {
+import repository.DAO;
+import domain.Domain;
 
+abstract class FileSystemDAO<T extends Domain<E>, E extends Throwable> implements DAO<T, E> {
+
+    @Override
+    public void inserir(T domain) throws E {
+        
+    }
 }
