8bd46c6bfd45b22e0f7fce0f8fea0d01e4be711a
Prestes, E. M <neoiron@gmail.com>
2015-06-25 00:14:01 -0300

    Criado comportamento 'openFile'.

--- repository-filesystem/src/main/java/repository/io/DataSource.java
+++ repository-filesystem/src/main/java/repository/io/DataSource.java
@@ -1,8 +1,25 @@
 package repository.io;
 
-public final class DataSource {
+import repository.exception.FileSystemException;
+
+import java.io.RandomAccessFile;
+
+final class DataSource {
+
+    interface Property {
+        String FILE_NAME = "io.properties";
+        String SOURCE = "io.source";
+
+        interface Permission {
+            String RW = "io.permission";
+        }
+    }
 
     private DataSource() {
         super();
+    }
+
+    public static RandomAccessFile openFile() throws FileSystemException {
+        return null;
     }
 }
