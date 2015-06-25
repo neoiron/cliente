f0089e5c9b3f8fd0eb5771cae84e1ffd464283ee
Prestes, E. M. <neoiron@gmail.com>
2015-06-25 12:57:34 -0300

    Incluídos comportamentos de 'MunicipioDAO'.

--- repository-filesystem/src/main/java/repository/io/FileSystemMunicipioDAO.java
+++ repository-filesystem/src/main/java/repository/io/FileSystemMunicipioDAO.java
@@ -1,5 +1,31 @@
 package repository.io;
 
-public class FileSystemMunicipioDAO {
+import java.util.Set;
 
+import repository.MunicipioDAO;
+import domain.exception.MunicipioException;
+import domain.model.Municipio;
+import domain.model.UFVO;
+
+public class FileSystemMunicipioDAO implements MunicipioDAO {
+
+    @Override
+    public void inserir(Municipio domain) throws MunicipioException {
+        
+    }
+
+    @Override
+    public void atualizar(Municipio domain) throws MunicipioException {
+
+    }
+
+    @Override
+    public void apagar(Municipio domain) throws MunicipioException {
+
+    }
+
+    @Override
+    public Set<Municipio> selecionar(UFVO uf) throws MunicipioException {
+        return null;
+    }
 }
