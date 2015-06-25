240fe2b61607ae0a426ac267db0a9067fc924728
Prestes, E. M. <neoiron@gmail.com>
2015-06-25 10:34:43 -0300

    Incluído 'serialVersionUID'.

--- repository-filesystem/src/main/java/repository/exception/FileSystemException.java
+++ repository-filesystem/src/main/java/repository/exception/FileSystemException.java
@@ -2,6 +2,8 @@ package repository.exception;
 
 public class FileSystemException extends Exception {
 
+    private static final long serialVersionUID = 3959814376569082473L;
+
     public FileSystemException(String message, Throwable cause) {
         super(message, cause);
     }
