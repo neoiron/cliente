e450df44bd1e4f225e07b3d3fb0f1d99c359d98f
Prestes, E. M. <neoiron@gmail.com>
2015-06-25 13:00:20 -0300

    Implementado comportamento 'inserir'.

--- repository-filesystem/src/main/java/repository/io/FileMunicipioDAO.java
+++ repository-filesystem/src/main/java/repository/io/FileMunicipioDAO.java
@@ -1,5 +1,7 @@
 package repository.io;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Set;
 
 import repository.MunicipioDAO;
@@ -7,11 +9,41 @@ import domain.exception.MunicipioException;
 import domain.model.Municipio;
 import domain.model.UFVO;
 
-public class FileMunicipioDAO implements MunicipioDAO {
+public class FileMunicipioDAO extends FileDAO implements MunicipioDAO {
+
+    public static final String FILE_NAME = "municipio.txt";
 
     @Override
     public void inserir(Municipio domain) throws MunicipioException {
-        
+        try (RandomAccessFile source = DataSource.openWriteableFile(FILE_NAME)) {
+            long size = source.length();
+            int line = 1;
+
+            if (size > 0) {
+                Municipio found;
+                String[] fields;
+                long pos;
+
+                source.seek(FILE_BEGIN);
+
+                do {
+                    fields = source.readLine().split(String.valueOf(CSV_SEPARATOR));
+                    found = new Municipio(fields[1], fields[2]);
+
+                    if (found.equals(domain)) {
+                        throw new MunicipioException("Município duplicado!");
+                    }
+
+                    pos = source.getFilePointer();
+                    ++line;
+                } while (pos < size);
+            }
+
+            domain.setId(line);
+            source.writeBytes(domain.toCSV(CSV_SEPARATOR));
+        } catch (IOException cause) {
+            throw new MunicipioException("PROBLEMAS AO INSERIR MUNICÍPIO!", cause);
+        }
     }
 
     @Override
