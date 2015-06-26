3f3ef88553be394084cb90b3869850dab3f890cc
Prestes, E. M. <neoiron@gmail.com>
2015-06-26 18:13:38 -0300

    Ajustes em 'HsqlDBHelper'.

--- repository-jdbc-hsqldb/src/main/java/repository/jdbc/hsqldb/HsqlDBHelper.java
+++ repository-jdbc-hsqldb/src/main/java/repository/jdbc/hsqldb/HsqlDBHelper.java
@@ -1,16 +1,27 @@
 package repository.jdbc.hsqldb;
 
+import org.hsqldb.Server;
+
 public final class HsqlDBHelper {
+
+    private static Server HSQL_SERVER = new Server();
 
     private HsqlDBHelper() {
         super();
     }
 
-    public static void startServer() {
-        
+    public static void startServer(String database) {
+        HSQL_SERVER.setLogWriter(null);
+        HSQL_SERVER.setSilent(true);
+        HSQL_SERVER.setDatabaseName(0, database);
+        HSQL_SERVER.setDatabasePath(0, "file:target/".concat(database));
+
+        HSQL_SERVER.start();
     }
 
     public static void stopServer() {
-        
+        if (HSQL_SERVER.isSilent()) {
+            HSQL_SERVER.stop();
+        }
     }
 }
