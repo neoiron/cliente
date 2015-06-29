77140ec7afea5b92656f8c24a1b1618546ee0fc2
Prestes, E. M. <neoiron@gmail.com>
2015-06-29 17:45:07 -0300

    Incluído o comportamento 'main'.

--- repository-jdbc-hsqldb/src/main/java/repository/jdbc/hsqldb/HsqlDBServer.java
+++ repository-jdbc-hsqldb/src/main/java/repository/jdbc/hsqldb/HsqlDBServer.java
@@ -1,5 +1,26 @@
 package repository.jdbc.hsqldb;
 
-public class HsqlDBServer {
+public final class HsqlDBServer {
 
+    public static void main(String[] args) {
+        String command, database;
+
+        if (args.length == 0)  {
+            System.out.println("Informe:");
+            System.out.printf("mvn exec:java <command> <database>");
+            return;
+        }
+
+        command = args[0];
+
+        switch (command.toLowerCase()) {
+        case "start":
+            database = args.length > 1 ? args[1] : "cliente";
+            HsqlDBHelper.startServer(database);
+            break;
+        case "stop":
+            HsqlDBHelper.stopServer();
+            break;
+        }
+    }
 }
