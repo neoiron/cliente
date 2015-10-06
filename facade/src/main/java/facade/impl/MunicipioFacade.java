6d55ff89da8e7c019a3b1c88f7c1a309e2f48b84
Prestes, E. M <neoiron@gmail.com>
2015-10-06 15:05:11 -0300

    Sobrecarregado comportamento 'validar'.

--- facade/src/main/java/facade/impl/MunicipioFacade.java
+++ facade/src/main/java/facade/impl/MunicipioFacade.java
@@ -8,6 +8,18 @@ public class MunicipioFacade {
 
     private MunicipioService service = new DefaultMunicipioService();
 
+    public void validar(CharSequence municipio, CharSequence uf) throws Exception {
+        validar(null, municipio, uf);
+    }
+
+    public void validar(CharSequence id, CharSequence municipio, CharSequence uf) throws Exception {
+        Municipio domain = new Municipio(municipio, uf);
+
+        domain.setId(id == null || "".equals(id) ? null : Integer.valueOf(id.toString()));
+
+        validar(domain);
+    }
+
     public void validar(Object municipio) throws Exception {
         service.validar((Municipio) municipio);
     }
