98a7748928a2b9fb05e837ff695150b3241730f3
Prestes, E. M <neoiron@gmail.com>
2015-10-06 14:34:09 -0300

    Criado comportamento 'listarUFs'.

--- facade/src/main/java/facade/impl/UFFacade.java
+++ facade/src/main/java/facade/impl/UFFacade.java
@@ -1,5 +1,13 @@
 package facade.impl;
 
+import domain.model.UFVO;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 public class UFFacade {
 
+    public Collection<UFVO> listarUFs() {
+        return Arrays.asList(UFVO.values());
+    }
 }
