04800278594daa888b7d6b467ac2bf252ea33dba
Prestes, E. M <neoiron@gmail.com>
2015-10-06 14:58:29 -0300

    Criado comportamento para validar município.

--- facade/src/main/java/facade/impl/MunicipioFacade.java
+++ facade/src/main/java/facade/impl/MunicipioFacade.java
@@ -1,5 +1,6 @@
 package facade.impl;
 
+import domain.model.Municipio;
 import service.MunicipioService;
 import service.impl.DefaultMunicipioService;
 
@@ -7,4 +8,7 @@ public class MunicipioFacade {
 
     private MunicipioService service = new DefaultMunicipioService();
 
+    public void validar(Object municipio) throws Exception {
+        service.validar((Municipio) municipio);
+    }
 }
