b9d801226d673c4bb639c966595a732c52b33c94
Prestes, E. M <neoiron@gmail.com>
2015-10-06 14:55:47 -0300

    Incluído service.

--- facade/src/main/java/facade/impl/MunicipioFacade.java
+++ facade/src/main/java/facade/impl/MunicipioFacade.java
@@ -1,5 +1,10 @@
 package facade.impl;
 
+import service.MunicipioService;
+import service.impl.DefaultMunicipioService;
+
 public class MunicipioFacade {
+
+    private MunicipioService service = new DefaultMunicipioService();
 
 }
