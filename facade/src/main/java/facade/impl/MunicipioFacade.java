f6986dc44b05f8f03a03ddb39b6905db57c96373
Prestes, E. M <neoiron@gmail.com>
2015-10-06 15:12:11 -0300

    Sobrecarregado comportamento 'salvar'.

--- facade/src/main/java/facade/impl/MunicipioFacade.java
+++ facade/src/main/java/facade/impl/MunicipioFacade.java
@@ -24,6 +24,18 @@ public class MunicipioFacade {
         service.validar((Municipio) municipio);
     }
 
+    public void salvar(CharSequence municipio, CharSequence uf) throws Exception {
+        salvar(null, municipio, uf);
+    }
+
+    public void salvar(CharSequence id, CharSequence municipio, CharSequence uf) throws Exception {
+        Municipio domain = new Municipio(municipio, uf);
+
+        domain.setId(id == null || "".equals(id) ? null : Integer.valueOf(id.toString()));
+
+        salvar(domain);
+    }
+
     public void salvar(Object municipio) throws Exception {
         service.salvar((Municipio) municipio);
     }
